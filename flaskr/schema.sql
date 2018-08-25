b4b618876816e08375f6c179c91622e32751b411
Kelly Savietta <ksavietta@gmail.com>
2018-08-25 16:29:59 -0500

    feat(schema) [KS] add name attrs

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -19,7 +19,8 @@ CREATE TABLE post (
 CREATE TABLE carbon_conversion_factor (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  property_name TEXT NOT NULL,
+  display_name TEXT NOT NULL,
+  api_name TEXT NOT NULL,
   input_units TEXT NOT NULL,
   input_to_co2 REAL NOT NULL,
   output_units TEXT NOT NULL,
